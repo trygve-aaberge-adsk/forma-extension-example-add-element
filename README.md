42fd6e2fa5c7c2224f51b22a658f022e7b218f14
Trygve Aaberge <trygve.aaberge@autodesk.com>
2023-12-07 11:06:17 +0100

    Add element when button is clicked

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Forma extensions example - add element
+
+This is an example of adding a simple mesh element to a proposal. It creates an element in the integrate element system by providing vertices and faces.
+
+The element has another element as a child. This is done to be able to translate the coordinates of the mesh in order to place the mesh at the elevation of the terrain. If a GLB file is used instead, the transform can be put inside the GLB, so then it may suffice to create a single element without any child.
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,69 @@
+import { Forma } from "forma-embedded-view-sdk/auto"
 import { render } from "preact"
 
 function App() {
+  const onClick = async () => {
+    const elevationAt0x0 = await Forma.terrain.getElevationAt({ x: 0, y: 0 })
+    const element = await Forma.integrateElements.createElementHierarchy({
+      data: {
+        rootElement: "element1",
+        elements: {
+          element1: {
+            id: "element1",
+            children: [
+              {
+                id: "element2",
+                transform: [
+                  1,
+                  0,
+                  0,
+                  0,
+                  0,
+                  1,
+                  0,
+                  0,
+                  0,
+                  0,
+                  1,
+                  0,
+                  0,
+                  0,
+                  elevationAt0x0,
+                  1,
+                ],
+              },
+            ],
+          },
+          element2: {
+            id: "element2",
+            properties: {
+              geometry: {
+                type: "Inline",
+                format: "Mesh",
+                verts: [
+                  -6, -6, 0, -6, 6, 0, 6, 6, 0, -6, -6, 0, 6, -6, 0, -6, 6, 12,
+                  -6, -6, 12, 6, -6, 12, 6, 6, 12,
+                ],
+                faces: [
+                  0, 1, 2, 3, 2, 4, 5, 6, 7, 5, 7, 8, 3, 6, 5, 3, 5, 1, 1, 5, 8,
+                  1, 8, 2, 2, 8, 7, 2, 7, 4, 4, 7, 6, 4, 6, 3,
+                ],
+              },
+            },
+          },
+        },
+      },
+    })
+    await Forma.proposal.addElement({ urn: element.urn })
+  }
+
   return (
     <weave-button
       onClick={() => {
-        console.log("clicked")
+        void onClick()
       }}
     >
-      Click me
+      Add element
     </weave-button>
   )
 }
